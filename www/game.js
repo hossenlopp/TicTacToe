9010855dd3fa88dbf204334ff870e841a16624a4
Chris Hossenlopp <hossenlopp@mitre.org>
2015-03-23 16:06:22 -0400

    added some java scripts

--- www/game.js
+++ www/game.js
@@ -1,13 +1,43 @@
 /**
- * Copyright (c) 2015 The MITRE Corporation. All rights reserved.
- *
- * NOTICE
- * This software was produced for the U.S. Government
- * under Contract No. FA8702-15-C-0001, and is
- * subject to the Rights in Noncommercial Computer Software
- * and Noncommercial Computer Software Documentation Clause
- * (DFARS) 252.227.7014 (JUN 1995)
- *
  * @authors Chris Hossenlopp <hossenlopp@mitre.org>
  * @date 3/23/2015
  */
+
+var board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+
+var turn = 'MA';
+var gameStatus = 'playing';
+
+
+$(document).ready(function () {
+  $(".cell").click(function (evt) {
+    var cellID = $(this).attr('id');
+    var row = cellID[5];
+    var col = cellID[7];
+    handleCellClick(row, col);
+  });
+});
+
+
+function handleCellClick(row, col) {
+  if (gameStatus === 'playing') {
+    if(board[row][col] === '') {
+      board[row][col] = turn;
+      $('#cell-' + row + '-' + col).append($('<img src="' + turn + '.svg"/>'));
+
+      if(turn === 'MA') {
+        turn = 'VA';
+        $('.status').text("Virginia's Turn!");
+      } else {
+        turn = 'MA';
+        $('.status').text("Massachusetts' Turn!");
+      }
+    } else {
+
+    }
+  }
+}
